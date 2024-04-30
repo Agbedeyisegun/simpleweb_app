350a41b880e3dfc374134e685e595ece7233af80
Agbedeyisegun <pheemyhan@gmail.com>
2024-04-30 14:08:47 +0100

    add color and increase font of the text in index file

--- index.js
+++ index.js
@@ -3,11 +3,27 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("He World, I am here for you, Devops Engineer");
-
+    // Add HTML styling for blue background and font size 20
+    const htmlResponse = `
+    <html>
+        <head>
+            <style>
+                body {
+                    background-color: blue;
+                    font-size: 40px;
+                    color: white; /* Set font color to white for better visibility */
+                }
+            </style>
+        </head>
+        <body>
+            <p>Hello World, I am here for you, Devops Engineer</p>
+        </body>
+    </html>
+    `;
+    res.send(htmlResponse);
 });
 
 app.listen(8080, () => {
     console.log('Listening on port 8080');
-
 });
+
